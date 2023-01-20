b4560f52b5f367b831c45f61c4b124ff28f6b3f7
Matheus Aragao <matheusaragaofs@gmail.com>
2023-01-20 17:37:36 -0300

    removing console.log

--- memory-game/src/helpers/cols-rows.js
+++ memory-game/src/helpers/cols-rows.js
@@ -3,13 +3,11 @@ import { getBoardIndexes } from "../boardIndexes";
 const getRandomIndex = (boardIndexes) =>
   Math.floor(Math.random() * boardIndexes.length);
 
-console.log('getBoardIndexes(8):', getBoardIndexes(9))
 
 export const getColsRows = (pbi) => {
   const index = getRandomIndex(pbi);
 
   if (!pbi[index]) {
-    console.log("1111111111");
     return getColsRows(pbi);
   }
   const [col, rows] = pbi[index];
