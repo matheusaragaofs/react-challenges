35b98fcb0c764f89975b343778fb44afa1e137f5
Matheus Aragao <matheusaragaofs@gmail.com>
2023-01-20 17:36:08 -0300

    getting the size of the boards

--- memory-game/src/App.jsx
+++ memory-game/src/App.jsx
@@ -1,94 +1,31 @@
 import { useState } from "react";
 import "./App.css";
-import { getEmoji, emojisLength } from "./emojis";
+import { Board } from "./components/Board";
 
 function App() {
-  const lengthArray = 4;
-  let board = new Array(lengthArray).fill("").map((e, index) => {
-    return new Array(lengthArray).fill(1);
-  });
+  const [boardSize, setBoardSize] = useState(2);
+  const [confirmSize, setConfirmSize] = useState(false);
 
-  let newCurrEmoji = getEmoji();
-  const getDiffEmojis = (board) => {
-    if (lengthArray ** 2 > emojisLength) return;
-    const emojiOccurrency = getOccurencesOfEmoji(board.flat(), newCurrEmoji);
-
-    if (emojiOccurrency == 2) {
-      newCurrEmoji = getEmoji();
-      if (board.flat().includes(newCurrEmoji)) getDiffEmojis(board);
-
-      return newCurrEmoji;
-    }
-    return newCurrEmoji;
-  };
-  const getOccurencesOfEmoji = (array, emoji) =>
-    array.filter((el) => el == emoji).length;
-
-  console.log("board:", board);
-  let boardIdexes = [];
-  for (let x = 0; x < lengthArray; x++) boardIdexes.push(x);
-
-  const getRandomIndex = (boardIndexes) =>
-    Math.floor(Math.random() * boardIndexes.length);
-
-  const bi = {
-    0: [0, 1, 2, 3],
-    1: [0, 1, 2, 3],
-    2: [0, 1, 2, 3],
-    3: [0, 1, 2, 3],
-  };
-  const pbi = Object.entries(bi);
-
-  const getColsRows = () => {
-    const index = getRandomIndex(pbi);
-    
-    if (!pbi[index]) {
-      return getColsRows();
-    }
-    const [col, rows] = pbi[index];
-
-    if (!rows.length) {
-      delete pbi[index];
-      return getColsRows();
-    }
-
-    const [randowRow] = rows.splice(getRandomIndex(rows), 1);
-
-    return {
-      col: Number(col),
-      row: randowRow,
-    };
-  };
-
-  const parsedBoardIndexes = {
-    col: boardIdexes,
-    row: boardIdexes,
-  };
-  console.log("pbi:", pbi);
-  board.forEach((col, indexCol) => {
-    col.forEach((_, indexRow) => {
-      const { row, col } = getColsRows();
-
-      board[row][col] = getDiffEmojis(board);
-    });
-  });
-
-  return (
-    <div className="App">
-      {board.map((col, i1) => (
-        <div key={i1} className="flex ">
-          {col.map((row, i2) => (
-            <div
-              key={i2}
-              className=" flex items-center select-none justify-center w-10 h-10  bg-white/20 m-1 hover:scale-110 transition-all duration-150  hover:bg-blue-300 cursor-pointer"
-            >
-              {row}
-            </div>
-          ))}
+  if (!confirmSize)
+    return (
+      <div>
+        Qual é o tamanho do board ?
+        <input
+          onChange={(e) => {
+            setBoardSize(Number(e.target.value));
+          }}
+          min={2}
+          max={7}
+          defaultValue={2}
+          type='range'
+        />
+        <div>
+          {boardSize} x {boardSize}
         </div>
-      ))}
-    </div>
-  );
+        <button onClick={() =>  setConfirmSize(true)}>Confirmar</button>
+      </div>
+    );
+  return <Board boardSize={boardSize} />;
 }
 
 export default App;
